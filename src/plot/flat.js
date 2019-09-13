dbc8a184f18169da6afaa00d395ff3a107f08b80
nazstone <nazstone@gmail.com>
2019-09-13 21:56:11 +0200

    fix: i18n and redraw on flat

--- src/plot/flat.js
+++ src/plot/flat.js
@@ -1,13 +1,72 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Line } from 'react-chartjs-2';
+import { withTranslation } from 'react-i18next';
 
-export default class flat extends React.Component {
+
+const transformPointsToData = (points) => {
+  if (!points || !points.length || points.length <= 0 ) return [];
+
+  const data = {
+      labels: points.map(p => {
+          return `${(`0${p.time.h}`).slice(-2)}:${(`0${p.time.m}`).slice(-2)}:${(`0${p.time.s}`).slice(-2)}`;
+      }),
+      datasets: [
+          {
+              label: 'Altitudes (pression)',
+              fill: false,
+              lineTension: 1,
+              backgroundColor: 'rgba(204,255,204,0.4)',
+              borderColor: 'rgba(204,255,204,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(204,255,204,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(204,255,204,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 1,
+              data: points.map(p => p.pressalt),
+          },
+          {
+              label: 'Altitudes (GPS)',
+              fill: false,
+              lineTension: 1,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 1,
+              data: points.map(p => p.gpsalt),
+          },
+      ],
+  };
+
+  return data;
+};
+
+class Flat extends React.Component {
     static propTypes = {
         className: PropTypes.string,
-
+        
         points: PropTypes.arrayOf(PropTypes.any),
-
+        
+        t: PropTypes.func.isRequired,
         onClick: PropTypes.func,
     }
 
@@ -23,7 +82,7 @@ export default class flat extends React.Component {
         super(props);
     
         this.state = {
-            data: this.transformPointsToData(props.points),
+            data: transformPointsToData(props.points),
         };
 
         this.clickHandler = this.clickHandler.bind(this);
@@ -35,66 +94,17 @@ export default class flat extends React.Component {
         this.tooltipLabelCallback = this.tooltipLabelCallback.bind(this);
         this.tooltipAfterLabelCallback = this.tooltipAfterLabelCallback.bind(this);
     }
-    
-    transformPointsToData(points) {
-        if (!points || !points.length || points.length <= 0 ) return [];
 
-        const data = {
-            labels: points.map(p => {
-                return `${(`0${p.time.h}`).slice(-2)}:${(`0${p.time.m}`).slice(-2)}:${(`0${p.time.s}`).slice(-2)}`;
-            }),
-            datasets: [
-                {
-                    label: 'Altitudes (pression)',
-                    fill: false,
-                    lineTension: 1,
-                    backgroundColor: 'rgba(204,255,204,0.4)',
-                    borderColor: 'rgba(204,255,204,1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: 'rgba(204,255,204,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: 'rgba(204,255,204,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 1,
-                    data: points.map(p => p.pressalt),
-                },
-                {
-                    label: 'Altitudes (GPS)',
-                    fill: false,
-                    lineTension: 1,
-                    backgroundColor: 'rgba(75,192,192,0.4)',
-                    borderColor: 'rgba(75,192,192,1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: 'rgba(75,192,192,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 1,
-                    data: points.map(p => p.gpsalt),
-                },
-            ],
-        };
-
-        return data;
+    static getDerivedStateFromProps(nextProps, prevState) {
+      return {
+          ...prevState,
+          data: transformPointsToData(nextProps.points),
+      };
     }
 
     tooltipTitleCallback(tooltipItem, data) {
         const { index } = tooltipItem[0];
-        return `Time : ${data.labels[index]}`;
+        return `${this.props.t('flat_time')}: ${data.labels[index]}`;
     }
 
     getPoint(index) {
@@ -112,21 +122,21 @@ export default class flat extends React.Component {
     tooltipBeforeLabelCallback(tooltipItem, data) {
         const point = this.getPoint(tooltipItem.index);
         const lines = [];
-        lines.push(`Altitude (pression) : ${point.pressalt.toFixed(2)}`)
-        lines.push(`Altitude (GPS) : ${point.gpsalt.toFixed(2)}`)
+        lines.push(`${this.props.t('flat_alt_pres')} : ${point.pressalt.toFixed(2)}`)
+        lines.push(`${this.props.t('flat_alt_gps')} : ${point.gpsalt.toFixed(2)}`)
         return lines.reduce(this.reducer);
     }
 
     tooltipLabelCallback(tooltipItem, data) {
         const point = this.getPoint(tooltipItem.index);
-        return `Latitude : ${point.lat.toFixed(4)}`;
+        return `${this.props.t('flat_latitude')} : ${point.lat.toFixed(4)}`;
     }
 
     tooltipAfterLabelCallback(tooltipItem, data) {
         const point = this.getPoint(tooltipItem.index);
         const lines = [];
-        lines.push(`Longitude : ${point.lng.toFixed(2)}`)
-        lines.push(`Speed : ${point.speed.toFixed(2)}`)
+        lines.push(`${this.props.t('flat_longitude')} : ${point.lng.toFixed(2)}`)
+        lines.push(`${this.props.t('flat_speed')} : ${point.speed.toFixed(2)}`)
         return lines.reduce(this.reducer);
     }
 
@@ -175,3 +185,5 @@ export default class flat extends React.Component {
         );
     }
 }
+
+export default withTranslation()(Flat);

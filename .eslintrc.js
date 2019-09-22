72c20a1fbb2e1150402139ffcf6bbdfb7caffff5
DUBUIS Michaël <Eldoran.s.e@gmail.com>
2019-09-22 22:59:48 +0200

    Improvement
    + full screen map
    + switch map and plot action
    + adding scale in map

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     "react/static-property-placement": [0],
     "react/destructuring-assignment": [0],
     "react/jsx-one-expression-per-line": [0],
-    "react/prefer-stateless-function": false,
+    "react/prefer-stateless-function": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "react/jsx-fragments": 0,
   }
 };
